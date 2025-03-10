c2e8313a8fa3b026906892f0e70fb246d62f78ae
idenyy <d3nys.pavlov@gmail.com>
2025-03-10 19:27:23 +0200

    refactor

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,4 +1,8 @@
-import { UseGuards } from "@nestjs/common";
-import { JwtGuard } from "../guards/jwt.guard";
+import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common'
+import { JwtGuard } from '../guards/jwt.guard'
+import { RolesGuard } from '../guards/role.guard'
 
-export const Auth = () => UseGuards(JwtGuard);
+export const ROLES_KEY = 'roles'
+export const Auth = (...roles: string[]) => {
+  return applyDecorators(SetMetadata(ROLES_KEY, roles), UseGuards(JwtGuard, RolesGuard))
+}
