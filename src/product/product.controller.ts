a860003a0ab91fa4f24f3b5abb7aad2b1c425149
idenyy <d3nys.pavlov@gmail.com>
2025-03-12 20:35:01 +0200

    feat: add product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,80 @@
-import { Controller } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
+import { ProductService } from './product.service'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { ProductDto, UpdateProductDto } from './dto/product.dto'
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async getAll(@Query('searchTerm') searchTerm?: string) {
+    return this.productService.getAll(searchTerm)
+  }
+
+  @Auth('MANAGER')
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.productService.getById(id)
+  }
+
+  @Get('category/:categoryId')
+  async getByCategory(@Param('categoryId') categoryId: string) {
+    return this.productService.getByCategory(categoryId)
+  }
+
+  @Get('supplier/:supplierId')
+  async getBySupplier(@Param('supplierId') supplierId: string) {
+    return this.productService.getBySupplier(supplierId)
+  }
+
+  @Get('popular')
+  async getMostPopular() {
+    return this.productService.getMostPopular()
+  }
+
+  @Get('similar/:id')
+  async getSimilar(@Param('id') id: string) {
+    return this.productService.getSimilar(id)
+  }
+
+  @Get(':id/rating')
+  async getAverageRating(@Param('id') id: string) {
+    return this.productService.getAverageRating(id)
+  }
+
+  @HttpCode(201)
+  @UsePipes(new ValidationPipe())
+  @Auth('MANAGER')
+  @Post()
+  async create(@Body() dto: ProductDto) {
+    return this.productService.create(dto)
+  }
+
+  @HttpCode(200)
+  @UsePipes(new ValidationPipe())
+  @Auth('MANAGER')
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
+    return this.productService.update(id, dto)
+  }
+
+  @HttpCode(200)
+  @Auth('MANAGER')
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.productService.remove(id)
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,168 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { ProductDto, UpdateProductDto } from './dto/product.dto'
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll(searchTerm?: string) {
+    if (searchTerm) return this.getSearchTermFilter(searchTerm)
+
+    return this.prisma.product.findMany({
+      orderBy: {
+        createdAt: 'desc'
+      },
+      include: {
+        category: true
+      }
+    })
+  }
+
+  async getById(id: string) {
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+      include: {
+        category: true,
+        users: true,
+        reviews: true
+      }
+    })
+
+    if (!product) throw new NotFoundException('Product not found')
+
+    return product
+  }
+
+  async getByCategory(categoryId: string) {
+    const products = await this.prisma.product.findMany({
+      where: { categoryId },
+      include: {
+        category: true
+      }
+    })
+
+    return products
+  }
+
+  async getBySupplier(supplierId: string) {
+    const products = await this.prisma.product.findMany({
+      where: { supplierId },
+      include: {
+        category: true
+      }
+    })
+
+    return products
+  }
+
+  async getMostPopular() {
+    const mostPopular = await this.prisma.orderItem.groupBy({
+      by: ['productId'],
+      _count: {
+        id: true
+      },
+      orderBy: {
+        _count: {
+          id: 'desc'
+        }
+      }
+    })
+
+    const productsIds = mostPopular.map((item) => item.productId)
+
+    const products = await this.prisma.product.findMany({
+      where: {
+        id: {
+          in: productsIds
+        }
+      },
+      include: {
+        category: true
+      }
+    })
+
+    return products
+  }
+
+  async getSimilar(id: string) {
+    const currentProduct = await this.getById(id)
+    if (!currentProduct) throw new NotFoundException('Current product not found')
+
+    const products = await this.prisma.product.findMany({
+      where: {
+        category: {
+          title: currentProduct.category.title
+        },
+        NOT: {
+          id: currentProduct.id
+        }
+      },
+      orderBy: {
+        createdAt: 'desc'
+      },
+      include: {
+        category: true
+      }
+    })
+
+    return products
+  }
+
+  async getAverageRating(productId: string) {
+    const reviews = await this.prisma.review.findMany({
+      where: { productId },
+      select: { rating: true }
+    })
+
+    if (reviews.length === 0) return 0
+
+    const total = reviews.reduce((acc, review) => acc + review.rating, 0)
+    return total / reviews.length
+  }
+
+  async create(dto: ProductDto) {
+    return this.prisma.product.create({
+      data: {
+        title: dto.title,
+        description: dto.description,
+        price: dto.price,
+        images: dto.images,
+        categoryId: dto.categoryId,
+        supplierId: dto.supplierId
+      }
+    })
+  }
+
+  async update(id: string, dto: UpdateProductDto) {
+    return this.prisma.product.update({
+      where: { id },
+      data: dto
+    })
+  }
+
+  async remove(id: string) {
+    return this.prisma.product.delete({ where: { id } })
+  }
+
+  private async getSearchTermFilter(searchTerm: string) {
+    return this.prisma.product.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: searchTerm,
+              mode: 'insensitive'
+            }
+          },
+          {
+            description: {
+              contains: searchTerm,
+              mode: 'insensitive'
+            }
+          }
+        ]
+      }
+    })
+  }
+}
