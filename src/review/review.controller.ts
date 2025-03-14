d3a4bf5ff51b6c0d900578488a08e493c1cf3b2a
idenyy <d3nys.pavlov@gmail.com>
2025-03-14 13:41:46 +0200

    feat: add review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
-import { ReviewService } from './review.service';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common'
+import { ReviewService } from './review.service'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { ReviewDto } from './dto/review.dto'
+import { CurrentUser } from '../user/decorators/user.decorator'
+import { Role } from '@prisma/client'
 
-@Controller('review')
+@Controller('reviews')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @HttpCode(201)
+  @UsePipes(new ValidationPipe())
+  @Auth()
+  @Post(':productId')
+  async create(@CurrentUser('id') userId: string, @Param('productId') productId: string, @Body() dto: ReviewDto) {
+    return this.reviewService.create(userId, productId, dto)
+  }
+
+  @Get()
+  async getAll() {
+    return this.reviewService.getAll()
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.reviewService.getById(id)
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Delete(':id')
+  async remove(@Param('id') id: string, @CurrentUser('id') userId: string, @CurrentUser('role') role: Role) {
+    return this.reviewService.remove(id, userId, role)
+  }
 }
