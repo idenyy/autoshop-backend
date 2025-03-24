8ea9d024e03d7c94e9b326033865cfb83bdd4958
idenyy <d3nys.pavlov@gmail.com>
2025-03-24 23:23:44 +0200

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,31 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { SupplierModule } from './supplier/supplier.module';
-import { OrderModule } from './order/order.module';
-import { ReviewModule } from './review/review.module';
-import { ProductModule } from './product/product.module';
-import { StatisticsModule } from './statistics/statistics.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+import { SupplierModule } from './supplier/supplier.module'
+import { OrderModule } from './order/order.module'
+import { ProductModule } from './product/product.module'
+import { StatisticsModule } from './statistics/statistics.module'
+import { CategoryModule } from './category/category.module'
+import { FileModule } from './file/file.module'
+import { ReviewModule } from './review/review.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule, SupplierModule, OrderModule, ReviewModule, ProductModule, StatisticsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    SupplierModule,
+    OrderModule,
+    ReviewModule,
+    ProductModule,
+    StatisticsModule,
+    CategoryModule,
+    FileModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
